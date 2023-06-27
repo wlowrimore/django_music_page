ab63a7759161e28df413bae67fc64e2c42f489f1
William Lowrimore <wlowrimore@gmail.com>
2023-06-26 22:37:55 -0500

    completes unstyled login and signup

--- personal_portfolio/urls.py
+++ personal_portfolio/urls.py
@@ -24,6 +24,8 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path('accounts/', include("django.contrib.auth.urls")),
     path("", TemplateView.as_view(template_name='home.html'), name='home'),
+    path("accounts/", include("accounts.urls")),
+    path("accounts/", include("django.contrib.auth.urls")),
     path("projects/", include("projects.urls")),
     path("blog/", include("blog.urls")),
 ]
