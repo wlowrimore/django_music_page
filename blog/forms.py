692fc030bde5e5456cd3e8659c138fea50ee1ee4
William Lowrimore <wlowrimore@gmail.com>
2023-07-10 20:15:53 -0500

    updates blog model with OneToMany relationship between user and comment for displaying author's username in comment posted.

--- blog/forms.py
+++ blog/forms.py
@@ -3,13 +3,8 @@ from users.models import CustomUser
 
 
 class CommentForm(forms.Form):
-    # author = forms.CharField(
-    #     max_length=60,
-    #     widget=forms.TextInput(attrs={
-    #         "class": "w-full outline-none border border-gray-300 rounded-sm my-4 py-1 px-2 w-full",
-    #         "placeholder": "Your Name"
-    #     })
-    # )
+    author = CustomUser.username
+
     body = forms.CharField(widget=forms.Textarea(
         attrs={
             "class": "w-full outline-none border border-gray-300 rounded-sm my-4 py-1 px-2 w-full",
--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,17 @@
-from users.models import CustomUser
+from personal_portfolio import settings
 from django.db import models
+
+User = settings.AUTH_USER_MODEL
+
+# class Snippet(models.Model):
+#     user = models.ForeignKey(User, default=1, null=True, on_delete=models.SET_NULL)
+#     blogname = models.CharField(max_length=100)
+#     blogauth = models.CharField(max_length=100)
+#     blogdes = models.TextField(max_length=400)
+#     img = models.ImageField(upload_to='pics')
+#
+#     def __str__(self):
+#         return self.blogname
 
 
 class Category(models.Model):
@@ -18,6 +30,7 @@ class Post(models.Model):
     created_on = models.DateTimeField(auto_now_add=True)
     last_modified = models.DateTimeField(auto_now=True)
     categories = models.ManyToManyField('Category', related_name='posts')
+    user = models.ForeignKey(User, default=1, null=True, on_delete=models.SET_NULL)
 
     def __str__(self):
         return self.title
